612fd53ce64972efb3b2dafd74c4fcabe2a8d5da
arturoneill <mobius0919@gmail.com>
2017-03-07 23:49:10 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ $ajaxUtils.sendGetRequest(
 
 // Builds HTML for the home page based on categories array
 // returned from the server.
-function buildAndShowHomeHTML (categories) {
+function buildAndShowHomeHTML(categories) {
 
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      var bet = "'" + chosenCategoryShortName+ "'";
+      var bet = "'" + "chosenCategoryShortName"+ "'";
        
        var homeHtmlToInsertIntoMainPage = insertProperty( homeHtml, "randomCategoryShortName", bet );
 
